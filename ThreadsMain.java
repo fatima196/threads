5be9226bd4e0692d2276d320a88227b157325001
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 18:03:37 +0200

    Update ThreadsMain.java

--- ThreadsMain.java
+++ ThreadsMain.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package threads;
 
 import static java.lang.Thread.MAX_PRIORITY;
 import static java.lang.Thread.MIN_PRIORITY;
 
-/**
- *
- * @author lenovo
- */
+
 public class ThreadsMain {
 
     /**
