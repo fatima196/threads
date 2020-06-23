b2dcc90258585b251d8437cc436670c75ee068d0
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 18:06:02 +0200

    Update thread1.java

--- thread1.java
+++ thread1.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package threads;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author lenovo
- */
+
 public class thread1 extends Thread{
     @Override
     public void run (){
