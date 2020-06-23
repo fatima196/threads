9a79021eb940b4e19cbbc14a48c2da1756e4222a
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 18:02:56 +0200

    Update StopWatch.java

--- StopWatch.java
+++ StopWatch.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package threads;
 
-/**
- *
- * @author lenovo
- */
+
 public class StopWatch extends Thread{
 
     public void​ start(){
